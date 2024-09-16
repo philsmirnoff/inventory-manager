35cf04efcf629b49b2ff4606d52e7b51808f5132
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-09-15 22:39:00 -0400

    set up navbar, fixed issue with tailwind config

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
