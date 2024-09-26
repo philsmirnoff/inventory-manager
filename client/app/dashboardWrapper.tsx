182ff7526ef0aabe30fb749ac3fe36771a70f2f3
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-09-25 23:39:05 -0400

    add baseline for sidebar

--- client/app/dashboardWrapper.tsx
+++ client/app/dashboardWrapper.tsx
@@ -1,9 +1,10 @@
+import Sidebar from "./(components)/Sidebar";
 import Navbar from "./(components)/Navbar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar by Me
+      <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
       >
